ee06f9f378a42203934a6a68b75264d13971497d
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-11-13 12:42:41 -0700

    added server and routes folder

---
+++ server.js
@@ -0,0 +1,30 @@
+// Add code to userModel.js to complete the model
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+//const User = require("./userModel.js");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", { useNewUrlParser: true });
+
+// // Routes
+
+require("./routes/html-routes.js")(app);
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
